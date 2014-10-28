ef76131faf230d81f1bdf3ec4ea7958457c1935e
whnmunp <wilsonnm22@gmail.com>
2014-10-28 00:30:48 -0600

    Delete IntConexionBD.java

--- CGV2.0/src/IntConexionBD.java
+++
@@ -1,8 +0,0 @@
-import java.rmi.*;
-
-public interface IntConexionBD  extends Remote
-{
-    public String EjecutarQuery(String query) throws RemoteException;
-    public void Conectar(String host, String BD, String User, String Password, String dbms) throws RemoteException;
-}
-
