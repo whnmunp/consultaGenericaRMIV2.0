e34e6ac833d9ba3efc67f0c12e2c96187e18183c
wilson <wilson@wilsonnm-pc>
2014-10-28 08:56:25 -0600

    WHNM 1:46 a ver si funciona

---
+++ src/ServidorRMI.java
@@ -0,0 +1,27 @@
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Alexandra
+ */
+public class ServidorRMI {
+    public static void main(String[] args) {
+         try {
+      //RMIREGISTRY
+      int puertoRMI=Integer.parseInt(JOptionPane.showInputDialog("Ingrese Puerto RMI"));
+      Registry registry = LocateRegistry.createRegistry(puertoRMI);
+
+      //Crear Objeto Servidor
+      ConexionBD conbd = new ConexionBD();
+
+      //Registrar Objeto Servidor
+      registry.rebind("conex", conbd);
+      System.out.println("Servidor Corriendo en el Puerto: "+puertoRMI);
+    } catch (Exception e) {
+      System.out.println(e);
+    }
+    }
+
+}
