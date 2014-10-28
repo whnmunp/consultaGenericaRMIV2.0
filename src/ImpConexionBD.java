2abd694f5cf2c005c0281d02ffca7714201ddb0f
wilson <wilson@wilsonnm-pc>
2014-10-28 10:27:25 -0600

    Espero que vaya a modificacion

--- src/ImpConexionBD.java
+++ src/ImpConexionBD.java
@@ -4,6 +4,7 @@
  * @author Alexandra
  */
 import java.rmi.*;
+//esta es una interfaz muy compleja en modificacion please
 public interface ImpConexionBD  extends Remote{
     public String EjecutarQuery(String query) throws RemoteException;
     public void Conectar(String host, String BD, String User, String Password, String dbms) throws RemoteException;
