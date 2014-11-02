25be320dc543698163d8f924b3e7fdefa77949f7
wilson <wilson@wilsonnm-pc>
2014-11-02 02:23:04 -0600

    Wilson Neira Mija

--- src/ImpConexionBD.java
+++ src/ImpConexionBD.java
@@ -1,11 +1,12 @@
 
 /**
  *
- * @author Alexandra
+ * @author Wilson&Carlos
  */
 import java.rmi.*;
+import javax.swing.JTable;
 //esta es una interface muy problematica
 public interface ImpConexionBD  extends Remote{
-    public String EjecutarQuery(String query) throws RemoteException;
+    public String EjecutarQuery(String query,JTable jtResultado) throws RemoteException;
     public void Conectar(String host, String BD, String User, String Password, String dbms) throws RemoteException;
 }
